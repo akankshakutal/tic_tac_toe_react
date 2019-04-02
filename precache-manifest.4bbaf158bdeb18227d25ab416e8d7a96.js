8ed4db961ff908472ad1b107cf94777d32dc7f30
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-04-02 11:44:35 +0530

    Updates

---
+++ precache-manifest.4bbaf158bdeb18227d25ab416e8d7a96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae372e8fcaba25e479ff",
+    "url": "/calculator_react/static/js/runtime~main.1f61ae32.js"
+  },
+  {
+    "revision": "453cd18933e4c0eebdcb",
+    "url": "/calculator_react/static/js/main.f8faaa88.chunk.js"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/calculator_react/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "453cd18933e4c0eebdcb",
+    "url": "/calculator_react/static/css/main.f19ac451.chunk.css"
+  },
+  {
+    "revision": "89bafb95163abbc35ea5e0a081c0f8b5",
+    "url": "/calculator_react/index.html"
+  }
+];
